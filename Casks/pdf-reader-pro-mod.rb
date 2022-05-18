593ac85affefe79c2febc357cb4aef620eccd24e
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-19 00:23:55 +0700

    Create pdf-reader-pro-mod.rb

---
+++ Casks/pdf-reader-pro-mod.rb
@@ -0,0 +1,24 @@
+cask "pdf-reader-pro-mod" do
+  version "2.8.10.1"
+  sha256 "d5b1ec358949fd15688af3dec109e0575206a5935311900a286655235751a498"
+
+  url "https://github.com/Zenfection/macos/releases/download/1.0/PDF.Reader.Pro_#{version}.dmg"
+  name "PDF Reader Pro"
+  desc "Read, annotate, edit, convert, create, OCR, fill forms and sign PDFs"
+  homepage "https://github.com/zenfection/macos"
+
+  livecheck do
+    url "https://www.pdfreaderpro.com/downloads/pdfreaderprocast.xml"
+    strategy :sparkle
+  end
+
+  app "PDF Reader Pro.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.brother.pdfreaderprofree.mac.sfl2",
+    "~/Library/Application Support/com.brother.pdfreaderprofree.mac",
+    "~/Library/Caches/com.brother.pdfreaderprofree.mac",
+    "~/Library/Preferences/com.brother.pdfreaderprofree.mac.bookmarks.plist",
+    "~/Library/Preferences/com.brother.pdfreaderprofree.mac.plist",
+  ]
+end
