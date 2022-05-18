8a25c7f9e35512a6307a2c2142cf0fcfef847612
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-18 18:25:50 +0700

    Update staruml-mod.rb

--- Casks/staruml-mod.rb
+++ Casks/staruml-mod.rb
@@ -1,5 +1,6 @@
 cask "staruml-mod" do
     version "5.0.1"
+    sha256 "2713053a90e93c10186071202cc0181ead92c33c43184b9a575def1b9d1d7333"
     url "https://github.com/Zenfection/macos/releases/download/1.0/StarUML_#{version}.dmg"
     name "StarUML"
     desc "Software modeler"
