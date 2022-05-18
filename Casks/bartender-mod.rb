253fa9e42b5213f2e4714b9ac32efb6f4992a3de
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-18 18:02:58 +0700

    Create bartender-mod.rb

---
+++ Casks/bartender-mod.rb
@@ -0,0 +1,33 @@
+cask "bartender-mod" do
+  version "4.1.50"
+  sha256 "1974c55307240fe0bb1569553937423ed1e192c222fef47d7d3fd570701fd8d5"
+  url "https://github.com/Zenfection/macos/releases/download/1.0/Bartender.#{version}.dmg"
+
+  name "Bartender"
+  desc "Menu bar icon organizer"
+  homepage "https://www.macbartender.com/"
+
+  livecheck do
+    url "https://www.macbartender.com/B2/updates/AppcastB#{version.major}.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+
+  app "Bartender #{version.major}.app"
+
+  uninstall delete:    [
+    "/Library/Audio/Plug-Ins/HAL/BartenderAudioPlugIn.plugin",
+    "/Library/PrivilegedHelperTools/com.surteesstudios.Bartender.BartenderInstallHelper",
+    "/Library/ScriptingAdditions/BartenderHelper.osax",
+    "/System/Library/ScriptingAdditions/BartenderSystemHelper.osax",
+  ],
+            launchctl: "com.surteesstudios.Bartender.BartenderInstallHelper",
+            quit:      "com.surteesstudios.Bartender"
+
+  zap trash: [
+    "~/Library/Caches/com.surteesstudios.Bartender",
+    "~/Library/Cookies/com.surteesstudios.Bartender.binarycookies",
+    "~/Library/Preferences/com.surteesstudios.Bartender.plist",
+  ]
+end
