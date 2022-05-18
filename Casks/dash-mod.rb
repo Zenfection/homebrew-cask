0fe37d6d6d9d123189e504d7c5b5c2f91b1beb8f
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-19 00:21:49 +0700

    Update dash-mod.rb

--- Casks/dash-mod.rb
+++ Casks/dash-mod.rb
@@ -1,6 +1,6 @@
 cask "dash-mod" do
     version "6.2.4"
-    sha256 ""
+    sha256 "67245476d124eb7e29d5bc183590270112aff21a67d257328e3c7b11c20da737"
     url "https://github.com/Zenfection/macos/releases/download/1.0/Dash_#{version}.dmg"
     name "Dash"
     desc "API documentation browser and code snippet manager"
