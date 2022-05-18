5c7609dbbaddc35ac35c91ea72400edc79bbb9be
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-18 23:36:09 +0700

    Create image2icon-mod.rb

---
+++ Casks/image2icon-mod.rb
@@ -0,0 +1,25 @@
+cask "image2icon" do
+  version "2.17"
+  sha256 "e9cded557920710cfbe99a553ec4f7a156cb066d848d5c53f92d54afba22c149"
+  url "https://github.com/Zenfection/macos/releases/download/1.0/Image2icon_#{version}.dmg"
+  name "Image2Icon"
+  desc "Icon creator and file and folder customizer"
+  homepage "https://github.com/zenfection/macos"
+
+  livecheck do
+    url "http://apps.shinynode.com/apps/image2icon_appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates false
+
+  app "Image2Icon.app"
+
+  zap trash: [
+    "~/Library/Caches/net.shinyfrog.image2icon",
+    "~/Library/Preferences/net.shinyfrog.image2icon.plist",
+    "~/Library/Containers/net.shinyfrog.image2icon*",
+    "~/Library/Containers/net.shinyfrog.templateRenderer",
+    "~/Library/Saved Application State/net.shinyfrog.image2icon.savedState",
+  ]
+end
