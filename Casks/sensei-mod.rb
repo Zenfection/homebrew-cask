4d4e9733f3bf6c3bdbfd93f0caca0ea5ed73c090
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-19 00:27:47 +0700

    Create sensei-mod.rb

---
+++ Casks/sensei-mod.rb
@@ -0,0 +1,36 @@
+cask "sensei-mod" do
+    version "1.5.2"
+    sha256 "cf8ce1e35b6578929bfd87f651cd05ce78f86df639a1632404784d06bd09b4af"
+  
+    url "https://github.com/Zenfection/macos/releases/download/1.0/Sensei_#{version}.dmg"
+    name "Sensei"
+    desc "Monitors the computer system and optimizes its performance"
+    homepage "https://github.com/zenfection"
+  
+    livecheck do
+      url "https://api.appcenter.ms/v0.1/public/sparkle/apps/51fc066a-f4b4-49ec-b966-b2f476d2eede"
+      strategy :sparkle
+    end
+  
+    auto_updates true
+    depends_on macos: ">= :catalina"
+  
+    app "Sensei.app"
+  
+    uninstall delete:    [
+      "/Library/LaunchAgents/org.cindori.SenseiMonitor.plist",
+      "/Library/LaunchDaemons/org.cindori.SenseiDaemon.plist",
+      "/Library/LaunchDaemons/org.cindori.SenseiHelper.plist",
+      "/Library/PrivilegedHelperTools/org.cindori.SenseiTool",
+    ],
+              launchctl: "org.cindori.SenseiTool"
+  
+    zap trash: [
+      "~/Library/Application Support/org.cindori.Sensei",
+      "~/Library/Application Support/Sensei",
+      "~/Library/Caches/org.cindori.Sensei",
+      "~/Library/Cookies/org.cindori.Sensei.binarycookies",
+      "~/Library/Preferences/org.cindori.Sensei.plist",
+      "~/Library/Saved Application State/org.cindori.Sensei.savedState",
+    ]
+  end
