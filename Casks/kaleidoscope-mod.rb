6a274bf08495e2c28db9fa7e400a988dc9ccd2d9
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-18 23:04:29 +0700

    Create kaleidoscope-mod.rb

---
+++ Casks/kaleidoscope-mod.rb
@@ -0,0 +1,36 @@
+cask "kaleidoscope-mod" do
+  version "3.4.1"
+  sha256 "f117a986f4d5b74f8fa38483b6df197c5f65c43b37d1864d70569a067a67ee4c"
+  url "https://github.com/Zenfection/macos/releases/download/1.0/Kaleidoscope_#{version}.dmg"
+  name "Kaleidoscope"
+  desc "Spot and merge differences in text and image files or folders"
+  homepage "https://github.com/zenfection/macos"
+
+  livecheck do
+    url "https://updates.kaleidoscope.app/v#{version.major}/prod/appcast"
+    regex(/Kaleidoscope[._-]v?(\d+(?:\.\d+)+)[._-](\d+)\.app\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
+
+  auto_updates false
+  depends_on macos: ">= :big_sur"
+
+  app "Kaleidoscope.app"
+  binary "#{appdir}/Kaleidoscope.app/Contents/Resources/bin/ksdiff"
+
+  zap trash: [
+    "~/Library/Application Support/app.kaleidoscope.v*",
+    "~/Library/Application Support/com.blackpixel.kaleidoscope",
+    "~/Library/Application Support/Kaleidoscope",
+    "~/Library/Caches/app.kaleidoscope.v*",
+    "~/Library/Caches/com.blackpixel.kaleidoscope",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.blackpixel.kaleidoscope",
+    "~/Library/Preferences/app.kaleidoscope.v*.plist",
+    "~/Library/Preferences/com.blackpixel.kaleidoscope.plist",
+    "~/Library/Saved Application State/app.kaleidoscope.v*.savedState",
+    "~/Library/Saved Application State/com.blackpixel.kaleidoscope.savedState",
+    "~/Library/WebKit/app.kaleidoscope.v*",
+  ]
+end
