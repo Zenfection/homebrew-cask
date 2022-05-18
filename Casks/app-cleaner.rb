0f59bf51e0e7c195c27f684b0edb7a3d7993d55e
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-19 00:29:45 +0700

    Create app-cleaner.rb

---
+++ Casks/app-cleaner.rb
@@ -0,0 +1,27 @@
+cask "app-cleaner-mod" do
+    version "7.7.1"
+    sha256 "764437156b744dc0b65950fef0ec1c4ecc764da9d0022143fa920fefcf07b512"
+  
+    url "https://github.com/Zenfection/macos/releases/download/1.0/App.Cleaner_#{version}.dmg"
+    name "Nektony App Cleaner & Uninstaller"
+    desc "Uninstaller and cleaning assistant"
+    homepage "https://nektony.com/mac-app-cleaner"
+  
+    livecheck do
+      url "https://nektony.com/pro-support/app-cleaner/update/update.xml"
+      strategy :sparkle
+    end
+  
+    depends_on macos: ">= :yosemite"
+  
+    app "App Cleaner #{version.major}.app"
+  
+    zap trash: [
+      "~/Library/Application Scripts/com.nektony.App-Cleaner-Pro",
+      "~/Library/Application Support/com.nektony.App-Cleaner-Pro",
+      "~/Library/Caches/com.nektony.App-Cleaner-Pro",
+      "~/Library/Containers/com.nektony.StartupDiskFull",
+      "~/Library/Preferences/com.nektony.App-Cleaner-Pro.plist",
+      "~/Library/Saved Application State/com.nektony.App-Cleaner-Pro.savedState",
+    ]
+  end
