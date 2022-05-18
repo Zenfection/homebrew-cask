01e0248c6bf1b5a50129596e09771eae2eba2e0c
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-18 23:18:36 +0700

    Create wifi-explorer-pro-mod.rb

---
+++ Casks/wifi-explorer-pro-mod.rb
@@ -0,0 +1,34 @@
+cask "wifi-explorer-pro-mod" do
+  version "3.4.5"
+  sha256 "dc7527b46072d01831357ae75e731d2a2620ba30189ea7b5908c0c50116ed15c"
+  url "https://github.com/Zenfection/macos/releases/download/1.0/WiFi.Explorer.Pro.#{version.major}_#{version}.dmg"
+  name "WiFi Explorer Pro"
+  desc "Scan, monitor, and troubleshoot wireless networks"
+  homepage "https://github.com/zenfection/macos"
+
+  livecheck do
+    url "https://www.intuitibits.com/appcasts/com.intuitibits.wifiexplorerpro#{version.major}cast.xml"
+    strategy :sparkle
+  end
+
+  depends_on macos: ">= :high_sierra"
+
+  pkg "WiFiExplorerPro_#{version.csv.first}.pkg"
+
+  uninstall pkgutil:   [
+    "com.intuitibits.wifiexplorerpro#{version.major}.pkg",
+    "com.intuitibits.wifiexplorerpro-helper.pkg",
+  ],
+            launchctl: [
+              "com.intuitibits.wifiexplorerpro#{version.major}.wifiexplorerpro-bpf",
+            ]
+
+  zap trash: [
+    "~/Library/Application Support/WiFi Explorer Pro #{version.major}",
+    "~/Library/Caches/com.apple.helpd/Generated/com.intuitibits.wifiexplorerpro#{version.major}.*",
+    "~/Library/Caches/com.intuitibits.wifiexplorerpro#{version.major}",
+    "~/Library/Group Containers/2B9R362QNU.com.adriangranados.wifiexplorer",
+    "~/Library/Preferences/com.intuitibits.wifiexplorerpro#{version.major}.plist",
+    "~/Library/Saved Application State/com.intuitibits.wifiexplorerpro#{version.major}.savedState",
+  ]
+end
