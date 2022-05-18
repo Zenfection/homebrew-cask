c6a4918276b8cd42fe73c233291e8d2af7753486
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-18 22:46:16 +0700

    Create sitesucker-pro-mod.rb

---
+++ Casks/sitesucker-pro-mod.rb
@@ -0,0 +1,24 @@
+cask "sitesucker-pro-mod" do
+  version "4.4.1"
+  sha256 "f305490236c509dea6c27af661e2c2d00e27b609bb51ff9500b5385c390ded08"
+  url "https://github.com/Zenfection/macos/releases/download/1.0/SiteSuckerPRO_#{version}.dmg"
+
+  name "SiteSucker Pro"
+  desc "Website downloader tool"
+  homepage "https://ricks-apps.com/osx/sitesucker/index.html"
+
+  livecheck do
+    url "https://ricks-apps.com/osx/sitesucker/history.html"
+    regex(/Version\s*(\d+(?:\.\d+)+)/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :mojave"
+
+  app "SiteSucker Pro.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/us.sitesucker.mac.sitesucker-pro",
+    "~/Library/Containers/us.sitesucker.mac.sitesucker-pro",
+  ]
+end
