feceb0b5646b6813f950d8750dcf747b34cba422
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-18 17:49:47 +0700

    Create betterzip-mod.rb

---
+++ Casks/betterzip-mod.rb
@@ -0,0 +1,32 @@
+cask "betterzip" do
+  version "5.2"
+  sha256 "2ec5d068035fccf8de1d7c9936a398561726198030204c21079d5967b1dcb5a1"
+
+  url "https://github.com/Zenfection/macos/releases/download/1.0/BetterZip_#{version}.dmg",
+  name "BetterZip"
+  desc "Utility to create and modify archives"
+  homepage "https://macitbetter.com/"
+
+  livecheck do
+    url "https://macitbetter.com/BetterZip.zip"
+    strategy :header_match
+  end
+
+  auto_updates false
+  depends_on macos: ">= :high_sierra"
+
+  app "BetterZip.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.macitbetter.betterzip.findersyncextension",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.macitbetter.betterzip.sfl*",
+    "~/Library/Application Support/com.macitbetter.betterzip",
+    "~/Library/Caches/com.apple.helpd/Generated/com.macitbetter.betterzip.help*",
+    "~/Library/Caches/com.macitbetter.betterzip",
+    "~/Library/Containers/com.macitbetter.betterzip.findersyncextension",
+    "~/Library/Group Containers/79RR9LPM2N.group.com.macitbetter.betterzip",
+    "~/Library/Group Containers/79RR9LPM2N.group.com.macitbetter.betterzip-setapp",
+    "~/Library/Preferences/com.macitbetter.betterzip.plist",
+    "~/Library/Saved Application State/com.macitbetter.betterzip.savedState",
+  ]
+end
