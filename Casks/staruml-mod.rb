e9440fe7fc45ec29b20ccdb5db4df1f2cfbeaa36
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-18 18:24:28 +0700

    Create staruml-mod.rb

---
+++ Casks/staruml-mod.rb
@@ -0,0 +1,9 @@
+cask "staruml-mod" do
+    version "5.0.1"
+    url "https://github.com/Zenfection/macos/releases/download/1.0/StarUML_#{version}.dmg"
+    name "StarUML"
+    desc "Software modeler"
+    homepage "https://github.com/zenfection/macos"
+  
+    app "StarUML.app"
+end
