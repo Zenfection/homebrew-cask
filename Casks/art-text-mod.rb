e4c3994ca25efac1d917f8356d9a2d58b322c1df
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-19 12:02:02 +0700

    Update art-text-mod.rb

--- Casks/art-text-mod.rb
+++ Casks/art-text-mod.rb
@@ -1,6 +1,6 @@
 cask "art-text-mod" do
     version "4.1.3"
-    sha256 "7f51192f8829e6e7481f32a142c60c051e50ee9d8ea2ce6b8c08d195cf66540e"
+    sha256 "97068db98e3755f5937849025debcab71f36f130bc594e8a760a809d93152155"
     url "https://github.com/Zenfection/macos/releases/download/1.0/Art.Text.#{version.major}_#{version}.dmg"
     name "Art Text #{version.major}"
     desc "Art Text 4 is a graphic design software tuned for creating text effect and text style-based graphics. The app contains a multitude of text design presets and effects to help you create visually enhanced photo captions, logos, newsletter and flyer headings, presentations and even movie titles."
