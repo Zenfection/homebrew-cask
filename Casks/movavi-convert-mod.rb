83ec41d18b8ada290d7420b4c0e72ef5ff8b07d0
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-05-20 21:58:29 +0700

    Update movavi-convert-mod.rb

--- Casks/movavi-convert-mod.rb
+++ Casks/movavi-convert-mod.rb
@@ -1,6 +1,6 @@
 cask "movavi-convert-mod" do
-    version "22.3.0"
-    sha256 "976bc3e0abc13be50028f01a26693b47b219a7ee8e971274f718fe9f7858bdbf"
+    version "22.4.0"
+    sha256 "5b87b9339da4d451e8900906eaf7a4fa4fbae211d7f34a931b755a2d1807f5ef"
     url "https://github.com/Zenfection/macos/releases/download/1.0/Movavi.Video.Converter.#{version.major}.Premium_#{version}.dmg"
     name "Movavi Convert Premium"
     desc "Movavi Video Converter is a video and audio file converter. Convert video and audio on your Mac. Save videos for watching on your portable device. Extract soundtracks from video. And much more…"
