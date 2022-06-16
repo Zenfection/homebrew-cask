7c7bcac1bfd3a1b50fca8d7ec7236c5e73e55564
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-06-16 16:45:09 +0700

    Update sitesucker-mod.rb

--- Casks/sitesucker-mod.rb
+++ Casks/sitesucker-mod.rb
@@ -1,4 +1,4 @@
-cask "sitesucker-pro-mod" do
+cask "sitesucker-mod" do
   version "5.0"
   sha256 "1156e504ef2a17f139c8156d512f710c820f058bfece0aa8c81118ecc5353ae6"
   url "https://github.com/Zenfection/macos/releases/download/1.0/SiteSucker_#{version}.dmg"
