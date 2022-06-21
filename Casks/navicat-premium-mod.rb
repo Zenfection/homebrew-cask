64d2b03a7fb8ea9aa94e782cd34acaea0c3133e6
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-06-21 20:23:02 +0700

    update

--- Casks/navicat-premium-mod.rb
+++ Casks/navicat-premium-mod.rb
@@ -1,7 +1,7 @@
 
 cask "navicat-premium-mod" do
-    version "16.0.13"
-    sha256 "46a0ccaf16830b7ebd88543aeafb058227a25ba250e81c80434816a03370fad9"
+    version "16.0.14"
+    sha256 "f7b364f94283878e78d266a9278004d84378960c6f816ad568397fbdadc13737"
     url "https://github.com/Zenfection/macos/releases/download/1.0/Navicat.Premium_#{version}.dmg"
     name "Navicat Premium"
     desc "Database administration and development tool"
