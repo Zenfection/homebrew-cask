d2482696e30e187822ec11388a0f4d4e220b4441
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-06-24 23:07:13 +0700

    Create soundsource-mod.rb

---
+++ Casks/soundsource-mod.rb
@@ -0,0 +1,26 @@
+cask "soundsource-mod" do
+    version "5.5.2"
+    sha256 "da47c733357d2e61713f9c057cdd081c3241f1c92095b177d61bc69f171a98cb"
+
+    url "https://github.com/Zenfection/macos/releases/download/1.0/SoundSource_#{version}.dmg"
+    name "SoundSource"
+    desc "Sound and audio controller"
+    homepage "https://github.com/zenfection/macos"
+  
+    livecheck do
+      url "https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&system=1231&bundleid=com.rogueamoeba.soundsource&platform=osx&version=#{version.no_dots}8000"
+      strategy :sparkle
+    end
+  
+    auto_updates true
+    depends_on macos: ">= :high_sierra"
+  
+    app "SoundSource.app"
+  
+    zap trash: [
+      "~/Library/Application Support/SoundSource",
+      "~/Library/Caches/com.rogueamoeba.soundsource",
+      "~/Library/Preferences/com.rogueamoeba.soundsource.plist",
+      "~/Library/WebKit/com.rogueamoeba.soundsource",
+    ]
+  end
