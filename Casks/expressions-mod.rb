15b5d1ad44062c5497877d6b367653db2a8a8430
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-07-03 22:24:39 +0700

    update fix

--- Casks/expressions-mod.rb
+++ Casks/expressions-mod.rb
@@ -2,15 +2,10 @@ cask "expressions-mod" do
     version "1.3.5"
     sha256 "a0407cdcb205c38c303f1adf6b4c16db4731a0c660d5826244db20eb59c606f5"
   
-    url "https://www.apptorium.com/public/products/expressions/releases/Expressions_#{version}.zip"
+    url "https://github.com/Zenfection/macos/releases/download/1.0/Expressions_#{version}.dmg"
     name "Expressions"
     desc "Regular expressions manager app"
-    homepage "https://www.apptorium.com/expressions"
-  
-    livecheck do
-      url "https://www.apptorium.com/expressions/trial"
-      regex(%r{href=.*?/Expressions-(\d+(?:\.\d+)*)\.zip}i)
-    end
+    homepage "https://github.com/zenfection/macos"
   
     depends_on macos: ">= :el_capitan"
   
