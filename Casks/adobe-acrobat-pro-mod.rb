3b498b48be166ef888c81ad88a15b173bba84eea
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-07-10 12:36:33 +0700

    update

--- Casks/adobe-acrobat-pro-mod.rb
+++ Casks/adobe-acrobat-pro-mod.rb
@@ -1,8 +1,8 @@
 cask "adobe-acrobat-pro-mod" do
     version "22.001.20112"
-    sha256 "62ebd3c08b7bdac7b718155911ac7a96597900f53b28ea335dc298564d6d3db7"
-  
-    url "https://github.com/Zenfection/macos/releases/download/1.0/Acrobat_DC_Web_WWMUI_#{version}.dmg"
+    sha256 "4d6c284d19a0cfdfdbfb9bcc666a469fde953ee2a18a2070bf978c080f8d8c8e"
+
+    url "https://github.com/Zenfection/macos/releases/download/1.0/Acrobat_DC_Web_WWMUI_#{version}.zip"
     name "Adobe Acrobat Pro DC"
     desc "View, create, manipulate, print and manage files in Portable Document Format"
     homepage "https://github.com/zenfection/macos"
@@ -10,14 +10,14 @@ cask "adobe-acrobat-pro-mod" do
     auto_updates true
 
     pkg "Acrobat DC/Acrobat DC Installer.pkg"
-
+    artifact 'Acrobat', target: "/Applications/Adobe Acrobat DC/Adobe Acrobat.app/Contents/Frameworks/Acrobat.framework/Versions/Acrobat"
     uninstall pkgutil:   [
                 "com.adobe.acrobat.DC.*",
                 "com.adobe.PDApp.AdobeApplicationManager.installer.pkg",
                 "com.adobe.AcroServicesUpdater",
                 "com.adobe.armdc.app.pkg",
-              ],
-              launchctl: [
+            ],
+            launchctl: [
                 "Adobe_Genuine_Software_Integrity_Service",
                 "com.adobe.AAM.Startup-1.0",
                 "com.adobe.AAM.Updater-1.0",
@@ -25,14 +25,14 @@ cask "adobe-acrobat-pro-mod" do
                 "com.adobe.ARMDC.SMJobBlessHelper",
                 "com.adobe.ARMDCHelper.cc24aef4a1b90ed56a725c38014c95072f92651fb65e1bf9c8e43c37a23d420d",
                 "com.adobe.agsservice",
-              ],
-              delete:    "/Applications/Adobe Acrobat DC/"
-  
+            ],
+            delete:    "/Applications/Adobe Acrobat DC/"
+
     zap trash: [
-      "~/Library/Application Support/Adobe/Acrobat/",
-      "~/Library/Caches/Acrobat",
-      "~/Library/Caches/com.adobe.Acrobat.Pro",
-      "~/Library/Preferences/Adobe/Acrobat/",
-      "~/Library/Preferences/com.adobe.Acrobat.Pro.plist",
+        "~/Library/Application Support/Adobe/Acrobat/",
+        "~/Library/Caches/Acrobat",
+        "~/Library/Caches/com.adobe.Acrobat.Pro",
+        "~/Library/Preferences/Adobe/Acrobat/",
+        "~/Library/Preferences/com.adobe.Acrobat.Pro.plist",
     ]
   end
