e03ce780c0b958cc3e797d0be985b6122161d05e
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-07-10 12:07:13 +0700

    update

---
+++ Casks/adobe-acrobat-pro-mod.rb
@@ -0,0 +1,40 @@
+cask "adobe-acrobat-pro" do
+    version "22.001.20112"
+    sha256 "62ebd3c08b7bdac7b718155911ac7a96597900f53b28ea335dc298564d6d3db7"
+  
+    url "https://github.com/Zenfection/macos/releases/download/1.0/Acrobat_DC_Web_WWMUI_#{version}.dmg",
+        user_agent: :fake,
+        cookies:    { "MM_TRIALS" => "1234" }
+    name "Adobe Acrobat Pro DC"
+    desc "View, create, manipulate, print and manage files in Portable Document Format"
+    homepage "https://github.com/zenfection/macos"
+
+    auto_updates true
+
+    pkg "Acrobat DC/Acrobat DC Installer.pkg"
+
+    uninstall pkgutil:   [
+                "com.adobe.acrobat.DC.*",
+                "com.adobe.PDApp.AdobeApplicationManager.installer.pkg",
+                "com.adobe.AcroServicesUpdater",
+                "com.adobe.armdc.app.pkg",
+              ],
+              launchctl: [
+                "Adobe_Genuine_Software_Integrity_Service",
+                "com.adobe.AAM.Startup-1.0",
+                "com.adobe.AAM.Updater-1.0",
+                "com.adobe.ARMDC.Communicator",
+                "com.adobe.ARMDC.SMJobBlessHelper",
+                "com.adobe.ARMDCHelper.cc24aef4a1b90ed56a725c38014c95072f92651fb65e1bf9c8e43c37a23d420d",
+                "com.adobe.agsservice",
+              ],
+              delete:    "/Applications/Adobe Acrobat DC/"
+  
+    zap trash: [
+      "~/Library/Application Support/Adobe/Acrobat/",
+      "~/Library/Caches/Acrobat",
+      "~/Library/Caches/com.adobe.Acrobat.Pro",
+      "~/Library/Preferences/Adobe/Acrobat/",
+      "~/Library/Preferences/com.adobe.Acrobat.Pro.plist",
+    ]
+  end
