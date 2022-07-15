a9eba81c0267d89a18fc48036fad44d7169ebc9b
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-07-15 15:17:36 +0700

    update camtasia

--- Casks/camtasia-mod.rb
+++ Casks/camtasia-mod.rb
@@ -1,6 +1,6 @@
 cask "camtasia-mod" do
-    version "2022.0.3"
-    sha256 "7ac877bc245a58afabd6edd3447fe55ef4bfaaadd8b9e7cc2f6131b02e4fcdc8"
+    version "2022.0.4"
+    sha256 "b86135b2364ec6270fa96ecafa1149a5b1807b7f67816c465ed62c35ae72f7c1"
     url "https://github.com/Zenfection/macos/releases/download/1.0/Camtasia_#{version}.dmg"
     name "Camtasia"
     desc "Screen recorder and video editor"
