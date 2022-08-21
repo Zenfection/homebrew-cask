6a192aa7d5e70995c8cf49fccc0e5dd9d88aadeb
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-08-21 22:42:57 +0700

    fix

--- Casks/adobe-illustrator-mod.rb
+++ Casks/adobe-illustrator-mod.rb
@@ -2,7 +2,7 @@ cask "adobe-illustrator-mod" do
     version "26.4.1"
     sha256 "ec147277d5ecf744e566735566d8e2a851944f3ddb37926f2f9b67ae028c26a9"
 
-    url "https://github.com/Zenfection/macos/releases/download/1.0/Adobe.Illustrator_26.4.1.dmg"
+    url "https://github.com/Zenfection/macos/releases/download/1.0/Adobe.Illustrator_26.4.1.zip"
     name "Adobe Illustrator"
     desc "The industry-standard vector graphics software lets you create logos, icons, drawings, typography, and illustrations for print, web, video, and mobile."
     homepage "https://github.com/zenfection/macos"
