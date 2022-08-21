510b762977feb7a6ad3b7a08ff0fe8506430dcf4
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-08-21 23:13:48 +0700

    fix

--- Casks/adobe-illustrator-mod.rb
+++ Casks/adobe-illustrator-mod.rb
@@ -1,6 +1,6 @@
 cask "adobe-illustrator-mod" do
     version "26.4.1"
-    sha256 "ec147277d5ecf744e566735566d8e2a851944f3ddb37926f2f9b67ae028c26a9"
+    sha256 "3cf7fa0021a0f7ad15a55acd266c9cc69a29c6df26d0d5a84f472ef9b7660ca0"
 
     url "https://github.com/Zenfection/macos/releases/download/1.0/Adobe.Illustrator_26.4.1.dmg"
     name "Adobe Illustrator"
