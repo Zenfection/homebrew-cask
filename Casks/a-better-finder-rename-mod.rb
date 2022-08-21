2ddf81bfe1577e5255cff3635817b1e2f9d77e1d
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-08-21 14:25:33 +0700

    create better rename

---
+++ Casks/a-better-finder-rename-mod.rb
@@ -0,0 +1,27 @@
+cask "a-better-finder-rename-mod" do
+    version "11.51"
+    sha256 "011eb0201939d6d8b05b808d8ec6d8648327173cd6308cd2fce38ecd5c4cf2e2"
+  
+    url "https://github.com/Zenfection/macos/releases/download/1.0/A.Better.Finder.Rename.11_11.51.dmg"
+    name "A Better Finder Rename"
+    desc "Renamer for files, music and photos"
+    homepage "https://www.publicspace.net/ABetterFinderRename/"
+  
+    livecheck do
+      url "https://www.publicspace.net/app/signed_abfr#{version.major}.xml"
+      strategy :sparkle, &:version
+    end
+  
+    auto_updates true
+  
+    app "A Better Finder Rename #{version.major}.app"
+  
+    zap trash: [
+      "~/Library/Application Support/A Better Finder Rename #{version.major}",
+      "~/Library/Caches/com.apple.helpd/Generated/net.publicspace.abfr#{version.major}.help*",
+      "~/Library/Caches/net.publicspace.abfr#{version.major}",
+      "~/Library/Cookies/net.publicspace.abfr#{version.major}.binarycookies",
+      "~/Library/Preferences/net.publicspace.abfr#{version.major}.plist",
+      "~/Library/Saved Application State/net.publicspace.abfr#{version.major}.savedState",
+    ]
+  end
