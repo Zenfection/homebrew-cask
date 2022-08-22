a6f7dd3478f9a9b17cc6366e7ed12bdbb2c5f300
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-08-22 22:48:55 +0700

    create cleanshotX

---
+++ Casks/cleanshot-mod.rb
@@ -0,0 +1,34 @@
+cask "cleanshot-mod" do
+    version "3.1.1"
+    sha256 "5d78db4e92cdd99baee784b9578df575279ad6920cc6e45b940c22dac8032b2f"
+  
+    url "https://github.com/Zenfection/macos/releases/download/1.0/CleanShot.X_#{version}.dmg"
+    name "CleanShot"
+    desc "Screen capturing tool"
+    homepage "https://github.com/zenfection/macos"
+  
+  
+    auto_updates true
+    depends_on macos: ">= :sierra"
+  
+    app "CleanShot X.app"
+  
+    installer script: {
+        executable:   "install.sh",
+        args:         ["--mode=silent"],
+        sudo:         true,
+        print_stderr: false,
+    }
+
+    artifact 'pl.maketheweb.cleanshotx.plist', target: "~/Library/Preferences/com.fatcatsoftware.pledpro.plist"
+    
+    uninstall quit: "pl.maketheweb.cleanshotx"
+  
+    zap trash: [
+      "~/Library/Application Support/CleanShot",
+      "~/Library/Caches/pl.maketheweb.cleanshotx",
+      "~/Library/Caches/SentryCrash/CleanShot X",
+      "~/Library/Preferences/com.getcleanshot.app.plist",
+      "~/Library/Preferences/pl.maketheweb.cleanshotx.plist",
+    ]
+  end
