b35fbf99ac0da438e0ec4ff6815147776e01eaa8
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-08-23 23:02:17 +0700

    fix updaet

--- Casks/adobe-illustrator-mod.rb
+++ Casks/adobe-illustrator-mod.rb
@@ -1,18 +1,20 @@
 cask "adobe-illustrator-mod" do
     version "26.4.1"
-    sha256 "2aaaf8dde8f1e27f805e6aed019e25e4456c60832bd4de43589a4914323c41de"
+    sha256 "576d3109cdee81bb2541e60e6a736ff678c116fb95cbbf597c2bdc32e36693eb"
 
-    url "https://github.com/Zenfection/macos/releases/download/1.0/Adobe.Illustrator_#{version}.dmg"
+    url "https://github.com/Zenfection/macos/releases/download/1.0/Adobe.Illustrator_#{version}.zip"
     name "Adobe Illustrator"
     desc "The industry-standard vector graphics software lets you create logos, icons, drawings, typography, and illustrations for print, web, video, and mobile."
     homepage "https://github.com/zenfection/macos"
     
     auto_updates true
 
+    pkg "AntiCC_1.7.pkg"
+
     installer script: {
-        executable:   "#{staged_path}/Install.app/Contents/MacOS/Install",
+        executable:   "#{staged_path}/Illustrator/Install.app/Contents/MacOS/Install",
         args:         ["--mode=silent"],
-        sudo:         false,
+        sudo:         true,
         print_stderr: false,
     }
 
