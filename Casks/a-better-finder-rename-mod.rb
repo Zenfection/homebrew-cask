96c05b705bda3bcd8fb474f18e3015b1bd193c83
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-08-23 20:13:19 +0700

    fix

--- Casks/a-better-finder-rename-mod.rb
+++ Casks/a-better-finder-rename-mod.rb
@@ -2,14 +2,16 @@ cask "a-better-finder-rename-mod" do
     version "11.51"
     sha256 "011eb0201939d6d8b05b808d8ec6d8648327173cd6308cd2fce38ecd5c4cf2e2"
   
-    url "https://raw.githubusercontent.com/Zenfection/homebrew-cask/main/Installer/cleanshot-mod.sh"
-    "https://github.com/Zenfection/macos/releases/download/1.0/A.Better.Finder.Rename.11_#{version}.dmg" 
+    url "https://github.com/Zenfection/macos/releases/download/1.0/A.Better.Finder.Rename.11_#{version}.dmg" 
     name "A Better Finder Rename"
     desc "Renamer for files, music and photos"
     homepage "https://www.publicspace.net/ABetterFinderRename/"
 
     auto_updates true
     
+install script: {
+    executable: "https://raw.githubusercontent.com/Zenfection/homebrew-cask/main/Installer/cleanshot-mod.sh",
+}
     app "A Better Finder Rename #{version.major}.app"
   
     zap trash: [
@@ -20,4 +22,4 @@ cask "a-better-finder-rename-mod" do
       "~/Library/Preferences/net.publicspace.abfr#{version.major}.plist",
       "~/Library/Saved Application State/net.publicspace.abfr#{version.major}.savedState",
     ]
-  end
+end
---
+++ Installer/a-better-finder-rename-mod.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# tải file cài đặt dmg
