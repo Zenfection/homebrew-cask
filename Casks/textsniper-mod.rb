b2c61c15502f067cd5d8a5a349ea65f959234221
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-09-07 16:06:41 +0700

    fix

--- Casks/textsniper-mod.rb
+++ Casks/textsniper-mod.rb
@@ -2,7 +2,7 @@ cask "textsniper-mod" do
     version "1.8.0"
     sha256 "c5b928650a982e033d3da15959e026e8f17b5ba63702c6cea4b51bd38dc9360c"
   
-    url "https://github.com/Zenfection/macos/releases/download/1.0/TextSnipper_#{version}.dmg"
+    url "https://github.com/Zenfection/macos/releases/download/1.0/TextSniper_#{version}.dmg"
     name "textsniper"
     desc "Extract text from images and other digital documents in seconds"
     homepage "https://textsniper.app/"
