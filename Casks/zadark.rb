275c403637f2bcbd1a961971a7e1818e40f0708a
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-09-12 11:58:44 +0700

    fix

--- Casks/zadark.rb
+++ Casks/zadark.rb
@@ -1,6 +1,6 @@
 cask "zadark" do
-    version "4.11"
-    sha256 "84491996dd212984f760e992d70920ccaccf8a718222b2a95ed64c169f019f45"
+    version "4.12"
+    sha256 "a00c70027c3141a3d7c8b6304b79e6e808735326f3075e91447a01b8fc63130e"
     url "https://github.com/Zenfection/macos/releases/download/1.0/ZaDark_#{version}.zip"
     name "ZaDark"
     desc "ZaDark là tiện ích mã nguồn mở giúp kích hoạt Dark Mode cho Zalo trên PC và Trình duyệt Web."
