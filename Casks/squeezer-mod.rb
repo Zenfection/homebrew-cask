7850a823d4d2ad74643a4086609337eb12b007b0
Zenfection <kietgolx65234@gmail.com>
2022-09-27 14:27:06 +0700

    update

--- Casks/squeezer-mod.rb
+++ Casks/squeezer-mod.rb
@@ -1,4 +1,4 @@
-cask "squeezer" do
+cask "squeezer-mod" do
     version "2.5.2"
     sha256 "896a3495a875cedfe462bf36d97b58d241369cce1a0c2ff62091a2ee0d5f8233"
     url "https://github.com/Zenfection/macos/releases/download/1.0/Squeezer_#{version}.dmg"
