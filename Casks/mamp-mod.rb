bc59cc0ec206ed24f8b5a6061be2fc045e9145a4
Zenfection <kietgolx65234@gmail.com>
2022-09-30 14:18:18 +0700

    update

--- Casks/mamp-mod.rb
+++ Casks/mamp-mod.rb
@@ -1,7 +1,7 @@
 cask "mamp-mod" do
-  version "6.6.1"
-  sha256 :no_check
-  url "https://github.com/Zenfection/macos/releases/download/1.0/Mamp.pro_#{version}.pkg"
+  version "6.6.3"
+  sha256 "15652463ec8fef33ee3fd5d32477feacb10cbaa1694128eac45bbf63b044a8e5"
+  url "https://github.com/Zenfection/macos/releases/download/1.0/Mamp.Pro_#{version}.pkg"
   
   name "MAMP"
   desc "Web development solution with Apache, Nginx, PHP & MySQL"
@@ -10,7 +10,7 @@ cask "mamp-mod" do
   auto_updates true
   depends_on macos: ">= :sierra"
 
-  pkg "Mamp.pro_#{version}.pkg"
+  pkg "Mamp.Pro_#{version}.pkg"
 
   postflight do
     set_ownership ["/Applications/MAMP", "/Applications/MAMP PRO"]
