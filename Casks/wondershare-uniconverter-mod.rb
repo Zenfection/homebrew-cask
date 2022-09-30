c1adb9e5a6377bdc0928ac2bf84c50da5681b317
Zenfection <kietgolx65234@gmail.com>
2022-09-30 14:09:14 +0700

    fix

--- Casks/wondershare-uniconverter-mod.rb
+++ Casks/wondershare-uniconverter-mod.rb
@@ -1,5 +1,5 @@
 cask "wondershare-uniconverter-mod" do
-    version "12.5.4.17"
+    version "14.2.2.5"
     sha256 "6b769bfead42b8c306f5bf5f91065de6ed37927cea69fb034dbd5d2abf82fbd8"
   
     url "https://github.com/Zenfection/macos/releases/download/1.0/Wondershare.UniConverter_#{version}.dmg"
