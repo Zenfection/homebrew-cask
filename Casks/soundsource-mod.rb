9bc1c5ef387890442c53f909cec4897847911332
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2022-12-20 13:34:39 +0700

    update

--- Casks/soundsource-mod.rb
+++ Casks/soundsource-mod.rb
@@ -1,6 +1,6 @@
 cask "soundsource-mod" do
-    version "5.5.6"
-    sha256 "5855423ba9001157d2e6c527cba524731f2d3b6119c496667424c4701e56b368"
+    version "5.5.7"
+    sha256 "fc49b48bb6a569cb25c8df60cc3b70f8fad67f2f4ad2af227883d68453fb1c95"
 
     url "https://github.com/Zenfection/macos/releases/download/1.0/SoundSource_#{version}.dmg"
     name "SoundSource"
