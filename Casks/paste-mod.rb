1582dd78dd64b2ab5839eb8e6cf81f4e0ff7974a
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-01-07 15:18:05 +0700

    Update paste-mod.rb

--- Casks/paste-mod.rb
+++ Casks/paste-mod.rb
@@ -1,6 +1,6 @@
 cask "paste-mod" do
-    version "2.3.8"
-    sha256 "9488d037adc6544e4f63fca1149ede6a17c311bc21e6bd4a43de87124f8d7896"
+    version "3.1.5"
+    sha256 "0fac211c2fcd502c12834a5eee2bd51f040940caca1876b5d17889767fa791a5"
     url "https://github.com/Zenfection/macos/releases/download/1.0/Paste_#{version}.dmg"
     name "Paste"
     desc "Paste is a new way to copy and paste for your Mac. It keeps everything you’ve ever copied and lets you use your clipboard history anytime you need it. It recognizes and stores text, images, links, files, and any other type of content, and generates informative previews for easy browsing."
