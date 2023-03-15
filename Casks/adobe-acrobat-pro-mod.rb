dd5807d622d86555cc6a560afe422fa5ba9dbc9e
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-03-15 15:26:47 +0700

    update

--- Casks/adobe-acrobat-pro-mod.rb
+++ Casks/adobe-acrobat-pro-mod.rb
@@ -1,5 +1,5 @@
 cask "adobe-acrobat-pro-mod" do
-    version "23.001.20063"
+    version "22.002.20191"
     sha256 "7fb7e9dd7ef75971ebb02dfe2fe8a924f69671282f7ca0218863a92534743a38"
 
     url "https://github.com/Zenfection/macos/releases/download/1.0/Adobe.Acrobat.Pro.DC_#{version}.dmg"
@@ -7,39 +7,34 @@ cask "adobe-acrobat-pro-mod" do
     desc "View, create, manipulate, print and manage files in Portable Document Format"
     homepage "https://github.com/zenfection/macos"
 
-    livecheck do
-        url "https://armmf.adobe.com/arm-manifests/mac/AcrobatDC/acrobat/current_version.txt"
-        regex(/^(\d+(?:\.\d+)+)$/i)
-    end
-
-    pkg "Acrobat DC Installer.pkg"
+    auto_updates true
 
     uninstall pkgutil:   [
-              "com.adobe.acrobat.DC.*",
-              "com.adobe.AcroServicesUpdater",
-              "com.adobe.armdc.app.pkg",
-              "com.adobe.PDApp.AdobeApplicationManager.installer.pkg",
+                "com.adobe.acrobat.DC.*",
+                "com.adobe.PDApp.AdobeApplicationManager.installer.pkg",
+                "com.adobe.AcroServicesUpdater",
+                "com.adobe.armdc.app.pkg",
             ],
             launchctl: [
-              "Adobe_Genuine_Software_Integrity_Service",
-              "com.adobe.AAM.Startup-1.0",
-              "com.adobe.AAM.Updater-1.0",
-              "com.adobe.agsservice",
-              "com.adobe.ARMDC.Communicator",
-              "com.adobe.ARMDC.SMJobBlessHelper",
-              "com.adobe.ARMDCHelper.cc24aef4a1b90ed56a725c38014c95072f92651fb65e1bf9c8e43c37a23d420d",
+                "Adobe_Genuine_Software_Integrity_Service",
+                "com.adobe.AAM.Startup-1.0",
+                "com.adobe.AAM.Updater-1.0",
+                "com.adobe.ARMDC.Communicator",
+                "com.adobe.ARMDC.SMJobBlessHelper",
+                "com.adobe.ARMDCHelper.cc24aef4a1b90ed56a725c38014c95072f92651fb65e1bf9c8e43c37a23d420d",
+                "com.adobe.agsservice",
             ],
             delete:    "/Applications/Adobe Acrobat DC/"
 
-  zap trash: [
-    "~/Library/Application Support/Adobe/Acrobat/",
-    "~/Library/Caches/Acrobat",
-    "~/Library/Caches/com.adobe.Acrobat.Pro",
-    "~/Library/HTTPStorages/com.adobe.Acrobat.Pro",
-    "~/Library/HTTPStorages/com.adobe.Acrobat.Pro.binarycookies",
-    "~/Library/Preferences/Adobe/Acrobat/",
-    "~/Library/Preferences/com.adobe.Acrobat.Pro.plist",
-    "~/Library/Saved Application State/com.adobe.Acrobat.Pro.savedState",
-    "~/Library/WebKit/com.adobe.Acrobat.Pro",
-  ]
+    zap trash: [
+      "~/Library/Application Support/Adobe/Acrobat/",
+      "~/Library/Caches/Acrobat",
+      "~/Library/Caches/com.adobe.Acrobat.Pro",
+      "~/Library/HTTPStorages/com.adobe.Acrobat.Pro",
+      "~/Library/HTTPStorages/com.adobe.Acrobat.Pro.binarycookies",
+      "~/Library/Preferences/Adobe/Acrobat/",
+      "~/Library/Preferences/com.adobe.Acrobat.Pro.plist",
+      "~/Library/Saved Application State/com.adobe.Acrobat.Pro.savedState",
+      "~/Library/WebKit/com.adobe.Acrobat.Pro",
+    ]
 end
