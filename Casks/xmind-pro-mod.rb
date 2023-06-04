2be8f85700508ad2979917b0e220532858df29bb
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-06-04 13:20:09 +0700

    update

--- Casks/xmind-pro-mod.rb
+++ Casks/xmind-pro-mod.rb
@@ -1,7 +1,7 @@
 cask "xmind-pro-mod" do
-    version "22.11.3656"
-    sha256 "f85c1944bf9883472b357cfc38397e3c21cc4251843e3fbb235f4c57289e3179"
-    url "https://github.com/Zenfection/macos/releases/download/1.0/Xmind.Pro_#{version}.zip"
+    version "23.5"
+    sha256 "7708dcb252bbc459e1d5ca531799ee7da664ca1bb618e0f5d0226d386aed7ee7"
+    url "https://github.com/Zenfection/macos/releases/download/1.0/Xmind_#{version}.zip"
     name "XMind"
     desc "Mind mapping and brainstorming tool"
     homepage "https://www.xmind.net/"
--- Casks/zadark.rb
+++ Casks/zadark.rb
@@ -1,7 +1,7 @@
 cask "zadark" do
-    version "8.1"
-    sha256 "594bc676f60660346f1a95322c295aad9153a9976857411bb3c0aca4ff09a6e9"
-    url "https://github.com/Zenfection/macos/releases/download/1.0/ZaDark_#{version}.zip"
+    version "8.2"
+    sha256 "0b623bda5bc0bfc085c21951e040679be1a38918da7f28d07055fdfee6a38385"
+    url "https://github.com/Zenfection/macos/releases/download/1.0/ZaDark_#{version}.pkg"
     name "ZaDark"
     desc "ZaDark là tiện ích mã nguồn mở giúp kích hoạt Dark Mode cho Zalo trên PC và Trình duyệt Web."
     homepage "https://github.com/zenfection/macos"
@@ -9,7 +9,7 @@ cask "zadark" do
     auto_updates true
     depends_on macos: ">= :mojave"
 
-    app "ZaDark"
+    pkg: "ZaDark_#{version}.pkg"
 
     installer manual: "ZaDark"
 
