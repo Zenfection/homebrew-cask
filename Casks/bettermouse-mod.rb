d08d5792f11ea4e93531be3251422635fe9be0e2
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-07-02 08:30:52 +0700

    update

--- Casks/bettermouse-mod.rb
+++ Casks/bettermouse-mod.rb
@@ -1,6 +1,6 @@
 cask "bettermouse-mod" do
-  version "1.5"
-  sha256 "9054bd22dede37907a80e90a6d06988f9cab667402c17d942c3f419eb1e4f1b9"
+  version "1.5.3768"
+  sha256 "1f4f7d9082913d295c2741fee003aec82cdb0b4701ff0fdfa02382158cb3c1a3"
 
   url "https://github.com/Zenfection/macos/releases/download/1.0/BetterMouse_#{version}.dmg"
   name "BetterMouse"
