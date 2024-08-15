f1c68a867e0c31ba8dd4924fce2cc122f8682777
Zen Le <44715845+zenkiet@users.noreply.github.com>
2024-08-15 09:31:25 +0700

    update

---
+++ Casks/snagit-mod.rb
@@ -0,0 +1,33 @@
+cask "snagit-mod" do
+  version "2024.3.0"
+  sha256 "41538291688ecd355e2131fc0e2a8bd49c20b097800c2a31e3b206dba7ff3de0"
+
+  url "https://github.com/Zenfection/macos/releases/download/1.0/Snagit_#{version}.dmg"
+  name "Snagit"
+  desc "Screen capture software"
+  homepage "https://www.techsmith.com/screen-capture.html"
+
+  livecheck do
+    url "https://www.techsmith.com/api/v/1/products/getallversions/100"
+    strategy :json do |json|
+      json.map do |item|
+        next if item["Major"].blank? || item["Minor"].blank? || item["Maintenance"].blank?
+
+        "20#{item["Major"]}.#{item["Minor"]}.#{item["Maintenance"]}"
+      end
+    end
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Snagit #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Caches/com.TechSmith.Snagit*",
+    "~/Library/Group Containers/*.com.techsmith.snagit",
+    "~/Library/Preferences/com.TechSmith.Snagit*.plist",
+    "~/Library/Preferences/com.techsmith.snagit.capturehelper*.plist",
+    "~/Library/Saved Application State/com.TechSmith.Snagit*.savedState",
+    "~/Library/WebKit/com.TechSmith.Snagit*",
+  ]
+end
